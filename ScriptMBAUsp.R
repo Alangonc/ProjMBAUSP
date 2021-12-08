7aa15fee3fff09e7228be8caff7a3e1ebc542556
Alan Goncalves <36668746+Alangonc@users.noreply.github.com>
2021-12-08 08:27:53 -0300

    Remote Update in the file
    Add new Functions

--- ScriptMBAUsp.R
+++ ScriptMBAUsp.R
@@ -1,9 +1,12 @@
 # Vserão 1
-
 # Versão 2
-
 # Versão 3
 
+#--- PACKAGES --------------------------
 install.packages("tidyverse")
-
 library("tidyverse")
+
+#--- FUNCTIONS -------------------------
+mutate()
+rename()
+filter()
